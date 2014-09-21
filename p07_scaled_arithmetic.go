0216e7f479bdd3bd4f5bd785eaee87454fe4e237
ThomasBHickey <ThomasBHickey@gmail.com>
2014-09-21 14:31:50 -0400

    More scaled arithmetic

--- p07_scaled_arithmetic.go
+++ p07_scaled_arithmetic.go
@@ -17,7 +17,7 @@
 package metatrue
 
 import (
-    "bytes"
+	"bytes"
 	"fmt"
 	//"math/big"
 	//"strings"
@@ -50,13 +50,13 @@ func clear_arith() {
 }
 
 // s100 Originally planned on using Go's rational package.  But not too much
-// magic in it, and it looks as though there is substantial overhead in 
+// magic in it, and it looks as though there is substantial overhead in
 // using it.  int64 should avoid the normal overflow problems people run
 // into with MetaFont and still make it possible to match MetaFont's results
 
 // s101
 const (
-	quater_unit        = 040000  // 2^14 is 0.25
+	quarter_unit       = 040000  // 2^14 is 0.25
 	half_unit          = 0100000 // 2^15 is 0.5
 	three_quarter_unit = 0140000 // 3*2^14 is 0.75
 	unity              = 0200000 // 2^16 is 1.0
@@ -76,47 +76,130 @@ func round_decimals(k int) scaled {
 		//a = (a + dig[k]*two) / 10
 	}
 	//return (*scaled)(big.NewRat((a+1)>>1, unity))
-	return scaled((a+1)/2)
+	return scaled((a + 1) / 2)
 }
 
-// add this for easier testing
+// s103
+
+// added floatString for easier testing
 // The big.Rat.FloatString routine returned "0.66667" for 2/3's
 // this routine appears to return "0.66666"
 func (s scaled) floatString() string {
-// 	ts := (*big.Rat)(sc).FloatString(5)
-// 	if strings.HasSuffix(ts, ".00000") {
-// 		return ts[:len(ts)-6]
-// 	}
-// 	return ts
-    buf := []byte{}
-    buffer := bytes.NewBuffer(buf)
-    var delta scaled
-    if s<0 {
-        buffer.WriteByte('-')
-        //print_char('-')
-        s = -s
-    }
-    fmt.Fprintf(buffer, "%d", s/unity)
-    //print_int(s/unity)
-    s = 10*(s % unity)+5
-    if s!=5{
-        delta = scaled(10)
-        buffer.WriteByte('.')
-        //print_char('.')
-        for {
-            if delta>unity {
-                s = s + 0100000-(delta/2)
-            }
-            buffer.WriteByte(byte('0'+(s/unity)))
-            //print_char('0'+(s/unity))
-            s = 10*(s%unity)
-            delta = delta*10
-            if s<=delta { break}
-        }
-    }     
-    return buffer.String()
+	// 	ts := (*big.Rat)(sc).FloatString(5)
+	// 	if strings.HasSuffix(ts, ".00000") {
+	// 		return ts[:len(ts)-6]
+	// 	}
+	// 	return ts
+	buffer := bytes.NewBuffer([]byte{})
+	var delta scaled
+	if s < 0 {
+		buffer.WriteByte('-')
+		//print_char('-')
+		s = -s
+	}
+	fmt.Fprintf(buffer, "%d", s/unity)
+	s = 10*(s%unity) + 5
+	if s != 5 {
+		delta = scaled(10)
+		buffer.WriteByte('.')
+		for {
+			if delta > unity {
+				s = s + 0100000 - (delta / 2)
+			}
+			buffer.WriteByte(byte('0' + (s / unity)))
+			s = 10 * (s % unity)
+			delta = delta * 10
+			if s <= delta {
+				break
+			}
+		}
+	}
+	return buffer.String()
 }
 
-func print_scaled(s scaled){
-    print(s.floatString())
+func (s scaled) Print() {
+	print(s.floatString())
+}
+
+// s104
+func print_two(x, y scaled) {
+	print_char('(')
+	x.Print()
+	print_char(',')
+	y.Print()
+	print_char(')')
+}
+
+// s105
+const (
+	fraction_half  = 01000000000
+	fraction_one   = 02000000000
+	fraction_two   = 04000000000
+	fraction_three = 06000000000
+	fraction_four  = 010000000000
+)
+
+type fraction int64
+
+// s106
+const (
+	forty_five_deg  = 0264000000
+	ninety_deg      = 0550000000
+	one_eighty_deg  = 01320000000
+	three_sixty_deg = 02640000000
+)
+
+type angle int64
+
+// s107
+// not worrying about overflow
+func make_fraction(p, q int) fraction {
+	return fraction((fraction_two*p + q) / (2 * q))
+}
+
+// s109
+// still not worrying about overflow!
+func take_fraction(q int64, f fraction) int64 {
+	negative := f < 0
+	if q < 0 {
+		negative = !negative
+	}
+	if negative {
+		f = -f
+	}
+	// 	fmt.Printf("take_fraction negative: %s\n", negative)
+	// 	fmt.Printf("fraction_half %x\n", fraction_half)
+	// 	fmt.Printf("fraction_one  %x\n", fraction_one)
+	// 	fmt.Printf("f             %x\n", f)
+	// 	fmt.Printf("q             %x\n", q)
+	// 	fmt.Printf("f+fraction_half: %x\n", f+fraction_half)
+	// 	fmt.Printf("fraction_one:    %x\n", fraction_one)
+	// 	fmt.Printf("q*f+1/2): %x", q*int64(f)+fraction_half)
+	p := (q*int64(f) + fraction_half) / fraction_one
+	if negative {
+		p = -p
+	}
+	return p
+}
+
+// s112
+func take_scaled(q int64, f scaled) int64 {
+	negative := f < 0
+	if q < 0 {
+		negative = !negative
+	}
+	if negative {
+		f = -f
+	}
+	p := (q*int64(f) + half_unit) / unity
+	if negative {
+		p = -p
+	}
+	return p
+}
+
+// s114
+// not worrying about overflow
+func make_scaled(p, q int) scaled {
+	return scaled((two*p + q) / (2 * q))
 }
--- p07_test.go
+++ p07_test.go
@@ -19,7 +19,7 @@ package metatrue
 import (
 	//"math/big"
 	"testing"
-	//"fmt"
+	"fmt"
 )
 
 func TestX07(t *testing.T) {
@@ -27,18 +27,32 @@ func TestX07(t *testing.T) {
 	dig[1] = 2
 	dig[2] = 5
 	sc := round_decimals(3)
-	//fmt.Println("sc .125?", sc.floatString())
 	if sc.floatString() != "0.125" {
 		t.Error("expected 0.125, got", sc.floatString())
 	}
-	//fmt.Println("denom", rat.Denom())
-	//fmt.Println("FloatString on .125 rat", rat.FloatString(5))
-	//rat2 := big.NewRat(1, 3)
-	//fmt.Println("FloatString on 1/3", rat2.FloatString(10))
+
+}
+
+func TestS101(t *testing.T) {
+	if half_unit != 2*quarter_unit ||
+		three_quarter_unit != 3*quarter_unit ||
+		unity != 2*half_unit ||
+		two != 2*unity ||
+		three != 3*unity {
+		//fmt.Printf("1/4:%x, 1/2:%x, 3/4:%x, 1:%x",quarter_unit, half_unit, three_quarter_unit, unity)
+		t.Error()
+	}
+}
+
+func TestS095(t *testing.T) {
+	bi := (int64(1) << 31) - 1
+	if el_gordo != bi {
+		t.Error("El Gordo not right?", el_gordo, bi)
+	}
 }
 
 func TestS103(t *testing.T) {
-	sc := (scaled)(unity/3)
+	sc := (scaled)(unity / 3)
 	tsThird := sc.floatString()
 	//fmt.Println("floatString of 1/3", tsThird)
 	if tsThird != "0.33333" {
@@ -49,12 +63,60 @@ func TestS103(t *testing.T) {
 	if ts1 != "1" {
 		t.Error("Expected", "1", "got", ts1)
 	}
-	ts2Thirds := (scaled)(two/3).floatString()
+	ts2Thirds := (scaled)(two / 3).floatString()
 	if ts2Thirds != "0.66666" {
 		t.Error("Expected 0.66666, got", ts2Thirds)
 	}
-	tsneg := (scaled)(-two/3).floatString()
+	tsneg := (scaled)(-two / 3).floatString()
 	if tsneg != "-0.66666" {
-	    t.Error("expected -0.66666, got", tsneg)
+		t.Error("expected -0.66666, got", tsneg)
 	}
+}
+
+func TestS105(t *testing.T) {
+	if fraction_one != fraction_half*2 ||
+		fraction_two != fraction_one*2 ||
+		fraction_three != fraction_one+fraction_two ||
+		fraction_four != 2*fraction_two {
+		t.Error("fractions", fraction_half>>28, fraction_one>>28, fraction_two>>28, fraction_three>>28, fraction_four>>28)
+	}
+}
+
+func TestS106(t *testing.T) {
+	if forty_five_deg*2 != ninety_deg {
+		t.Error("2*45 != 90?", forty_five_deg>>20, ninety_deg>>20)
+	}
+	if one_eighty_deg != 2*ninety_deg ||
+		three_sixty_deg != 4*ninety_deg {
+		t.Error("Problem with degrees", one_eighty_deg>>20, three_sixty_deg>>20)
+	}
+}
+
+func TestS107(t *testing.T) {
+	if make_fraction(1, 2) != fraction_half {
+		t.Error("1/2 fraction not working")
+	}
+}
+
+func TestS109(t *testing.T) {
+	if take_fraction(2, fraction_half) != 1 {
+		t.Error("take_fraction problem", take_fraction(2, fraction_half))
+	}
+}
+
+func TestS112(t *testing.T) {
+	if take_scaled(1, unity) != 1 {
+		t.Error("take_scaled", take_scaled(1, unity))
+	}
+	if take_scaled(4, quarter_unit) != 1 {
+		t.Error("take_scaled expected 4", take_scaled(1, quarter_unit))
+	}
+}
+
+func TestS114(t *testing.T){
+    if make_scaled(1,2) != half_unit {
+        fmt.Printf("half_unit    %x\n", half_unit)
+        fmt.Printf("returned val %x\n", make_fraction(1,2))
+        t.Error("make_fraction", make_fraction(1,2))
+    }
 }
